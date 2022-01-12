1d1cf7b13736eef4a607986cccfa9417a2a65854
Olly <developer.olly@gmail.com>
2022-01-13 00:19:43 +0100

    longest common prefix

---
+++ leetcode/longestCommonPrefix.js
@@ -0,0 +1,17 @@
+var longestCommonPrefix = function (strs) {
+  let prefix = '';
+  let i = 0;
+  let min = Number.MAX_SAFE_INTEGER;
+
+  for (let i = 0; i < strs.length; i++) {
+    //  if()
+  }
+
+  while (i < strs.length - 1) {}
+
+  return prefix;
+};
+
+const strs = ['flower', 'flower', 'flower', 'flower'];
+
+console.log(longestCommonPrefix(strs));
