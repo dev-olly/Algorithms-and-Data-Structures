94b31dde4c686b2f28a9abbbbddb7d9e557ba942
Olly <developer.olly@gmail.com>
2022-01-22 22:37:15 +0100

    lc: minSubarray brute force 2

--- leetcode/minSubArrayLen.ts
+++ leetcode/minSubArrayLen.ts
@@ -1,36 +1,64 @@
+// My 2 pointer version
+
+// function minSubArrayLen(target: number, nums: number[]): number {
+//   if (nums.length === 0) {
+//     return 0;
+//   }
+//   let i: number = 0;
+//   let min: number = Number.MAX_VALUE;
+
+//   while (i < nums.length) {
+//     let sum = nums[i];
+//     if (sum >= target) {
+//       return 1;
+//     }
+//     let j = i + 1;
+//     while (sum < target && j < nums.length) {
+//       sum += nums[j];
+//       if (sum >= target) {
+//         let distance = j - i + 1;
+//         min = Math.min(min, distance);
+//         break;
+//       }
+//       j++;
+//     }
+//     i++;
+//   }
+
+//   if (min > nums.length) {
+//     min = 0;
+//   }
+
+//   return min;
+// }
+
 function minSubArrayLen(target: number, nums: number[]): number {
   if (nums.length === 0) {
     return 0;
   }
-  let i: number = 0;
+  let sum: number[] = new Array(nums.length).fill(0);
+  sum[0] = nums[0];
   let min: number = Number.MAX_VALUE;
 
-  while (i < nums.length) {
-    let sum = nums[i];
-    if (sum >= target) {
-      return 1;
-    }
-    let j = i + 1;
-    while (sum < target && j < nums.length) {
-      sum += nums[j];
-      if (sum >= target) {
-        let distance = j - i + 1;
-        min = Math.min(min, distance);
+  for (let i: number = 1; i < nums.length; i++) {
+    sum[i] = sum[i - 1] + nums[i];
+  }
+
+  for (let i: number = 0; i < nums.length; i++) {
+    for (let j: number = i; j < nums.length; j++) {
+      const s = sum[j] - sum[i] + nums[i];
+      if (s >= target) {
+        min = Math.min(min, j - i + 1);
+
         break;
       }
-      j++;
     }
-    i++;
   }
 
-  if (min > nums.length) {
-    min = 0;
-  }
-
-  return min;
+  return min === Number.MAX_VALUE ? 0 : min;
 }
 
-const nums = [1, 1, 1, 1, 1, 1, 1, 1];
-const target = 11;
+const nums = [2, 3, 1, 2, 4, 3];
+const target = 7;
 
 console.log(minSubArrayLen(target, nums));
