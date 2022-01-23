44f07b376076b7abcbf2760d7852f1f63ddd1b2b
Olly <developer.olly@gmail.com>
2022-01-23 21:45:54 +0100

    lc: delete node

---
+++ leetcode/LinkedList/deleteNode.ts
@@ -0,0 +1,15 @@
+// https://leetcode.com/problems/delete-node-in-a-linked-list/
+
+// class ListNode {
+//   val: number;
+//   next: ListNode | null;
+//   constructor(val?: number, next?: ListNode | null) {
+//     this.val = val === undefined ? 0 : val;
+//     this.next = next === undefined ? null : next;
+//   }
+// }
+
+function deleteNode(root: ListNode | null): void {
+  root.val = root.next.val;
+  root.next = root.next.next;
+}
