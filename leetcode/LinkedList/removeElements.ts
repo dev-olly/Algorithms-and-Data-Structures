06adbc0dda09289994703ee26fa561f9aa8dad6d
Olly <developer.olly@gmail.com>
2022-01-24 23:04:25 +0100

    lc: recursive method for remove Elements

--- leetcode/LinkedList/removeElements.ts
+++ leetcode/LinkedList/removeElements.ts
@@ -1,23 +1,32 @@
+// function removeElements(head: ListNode | null, val: number): ListNode | null {
+//   if (!head) {
+//     return null;
+//   }
+//   let node = head;
+//   let sentinel = new ListNode(0);
+//   sentinel.next = head;
+
+//   let prev = sentinel;
+
+//   while (node) {
+//     if (node.val === val) {
+//       // in between
+//       prev.next = node.next;
+//     } else {
+//       prev = node;
+//     }
+//     node = node.next;
+//   }
+
+//   return sentinel.next;
+// }
+
+// recursion
 function removeElements(head: ListNode | null, val: number): ListNode | null {
-  if (!head) {
-    return null;
-  }
-  let node = head;
-  let sentinel = new ListNode(0);
-  sentinel.next = head;
+  if (!head) return null;
+  const node = removeElements(head.next, val);
+  if (head.val === val) return node;
+  head.next = node;
 
-  let prev = sentinel;
-
-  while (node) {
-    if (node.val === val) {
-      // in between
-      prev.next = node.next;
-      node = node.next;
-    } else {
-      prev = node;
-      node = node.next;
-    }
-  }
-
-  return sentinel.next;
+  return head;
 }
