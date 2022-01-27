a564f43b8d46bac5a9c99f05978a5f08bbe14dae
Olly <developer.olly@gmail.com>
2022-01-27 22:00:46 +0100

    lc:delete middle node solved

---
+++ leetcode/LinkedList/deleteMiddle.ts
@@ -0,0 +1,45 @@
+class ListNode {
+  val: number;
+  next: ListNode | null;
+  constructor(val?: number, next?: ListNode | null) {
+    this.val = val === undefined ? 0 : val;
+    this.next = next === undefined ? null : next;
+  }
+}
+
+function deleteMiddle(head: ListNode | null): ListNode | null {
+  let length = 0;
+  let curr = head;
+
+  while (curr) {
+    length += 1;
+    curr = curr.next;
+  }
+
+  let sentinel = new ListNode(0);
+  sentinel.next = head;
+  const mid = Math.floor(length / 2);
+
+  let i = 0;
+
+  if (mid === 0) {
+    return null;
+  }
+
+  while (i != mid) {
+    console.log('here');
+
+    if (mid - i === 1) {
+      head.next = head.next.next;
+    } else {
+      head = head.next;
+    }
+    i++;
+  }
+
+  return sentinel.next;
+}
+
+const head = new ListNode(1);
+
+console.log(deleteMiddle(head));
