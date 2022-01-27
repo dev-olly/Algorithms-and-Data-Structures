643d1104b4625937c1a57614c8587dcbc23cc59b
Olly <developer.olly@gmail.com>
2022-01-27 23:17:49 +0100

    lc 2 pointer technique for solving deleteMiddle

--- leetcode/LinkedList/deleteMiddle.ts
+++ leetcode/LinkedList/deleteMiddle.ts
@@ -7,37 +7,57 @@ class ListNode {
   }
 }
 
+// function deleteMiddle(head: ListNode | null): ListNode | null {
+//   let length = 0;
+//   let curr = head;
+
+//   while (curr) {
+//     length += 1;
+//     curr = curr.next;
+//   }
+
+//   let sentinel = new ListNode(0);
+//   sentinel.next = head;
+//   const mid = Math.floor(length / 2);
+
+//   let i = 0;
+
+//   if (mid === 0) {
+//     return null;
+//   }
+
+//   while (i != mid) {
+//     if (mid - i === 1) {
+//       head.next = head.next.next;
+//     } else {
+//       head = head.next;
+//     }
+//     i++;
+//   }
+
+//   return sentinel.next;
+// }
+
+// 2 pointer technique
 function deleteMiddle(head: ListNode | null): ListNode | null {
-  let length = 0;
-  let curr = head;
-
-  while (curr) {
-    length += 1;
-    curr = curr.next;
-  }
-
-  let sentinel = new ListNode(0);
-  sentinel.next = head;
-  const mid = Math.floor(length / 2);
-
-  let i = 0;
-
-  if (mid === 0) {
+  if (!head.next) {
     return null;
   }
+  let sentinel = new ListNode(-1);
+  sentinel.next = head;
+  let slow = sentinel;
+  let fast = sentinel;
 
-  while (i != mid) {
-    if (mid - i === 1) {
-      head.next = head.next.next;
-    } else {
-      head = head.next;
-    }
-    i++;
+  while (fast.next && fast.next.next) {
+    slow = slow.next;
+    fast = fast.next.next;
   }
 
-  return sentinel.next;
+  slow.next = slow.next.next;
+
+  return head;
 }
 
-const head = new ListNode(1);
+const head = new ListNode(1, new ListNode(2, new ListNode(3, null)));
 
 console.log(deleteMiddle(head));
