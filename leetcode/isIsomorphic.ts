ac07994fa9cb8100b4d07de78f7a0867662d37fc
Olly <developer.olly@gmail.com>
2022-02-01 15:02:12 +0100

    lc: isIsomorphic

--- leetcode/isIsomorphic.ts
+++ leetcode/isIsomorphic.ts
@@ -3,11 +3,30 @@ function isIsomorphic(s: string, t: string): boolean {
     return false;
   }
   const hashtable = new Map<string, string>();
+  const thashtable = new Map<string, string>();
 
+  for (let i: number = 0; i < s.length; i++) {
+    if (hashtable.has(s[i])) {
+      if (hashtable.get(s[i]) !== t[i]) {
+        return false;
+      }
+    } else {
+      hashtable.set(s[i], t[i]);
+    }
+
+    // t[i]
+    if (thashtable.has(t[i])) {
+      if (thashtable.get(t[i]) !== s[i]) {
+        return false;
+      }
+    } else {
+      thashtable.set(t[i], s[i]);
+    }
+  }
   return true;
 }
 
-const s = 'foo';
-const t = 'bar';
+const s = 'paper';
+const t = 'title';
 
 console.log(isIsomorphic(s, t));
