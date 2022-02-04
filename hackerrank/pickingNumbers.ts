62520d78d819c527f38b890c53dd8e16311d63d8
Olly <developer.olly@gmail.com>
2022-02-04 23:46:39 +0100

    hr: picking numbers

---
+++ hackerrank/pickingNumbers.ts
@@ -0,0 +1,23 @@
+function pickingNumbers(a: number[]): number {
+  // Write your code here
+  a.sort((a, b) => a - b);
+  let result = 0;
+  let count = 1;
+  let sub = a[0]; // subsequence first item
+
+  for (let i: number = 1; i < a.length; i++) {
+    if (a[i] === sub || sub + 1 === a[i]) {
+      count++;
+    } else {
+      result = Math.max(result, count);
+      sub = a[i];
+      count = 1;
+    }
+  }
+
+  return Math.max(result, count);
+}
+
+const a = [1, 1, 2, 2, 4, 4, 5, 5, 5];
+
+console.log(pickingNumbers(a));
