16c8a10518dddc6317daff9580b3ca69442f3200
Olly <developer.olly@gmail.com>
2022-02-06 21:52:12 +0100

    lc: bruteforce canpermutepalindrome

--- leetcode/canPermutePalindrome.ts
+++ leetcode/canPermutePalindrome.ts
@@ -1,20 +1,35 @@
+// Set
+// function canPermutePalindrome(s: string): boolean {
+//   // racecar - race
+//   const hashset = new Set();
+
+//   for (let i: number = 0; i < s.length; i++) {
+//     if (hashset.has(s.charAt(i))) {
+//       hashset.delete(s.charAt(i));
+//     } else {
+//       hashset.add(s.charAt(i));
+//     }
+//   }
+
+//   return hashset.size >= 1;
+// }
+
+// Brute-force
 function canPermutePalindrome(s: string): boolean {
-  // racecar - race
-  const hashset = new Set();
-
-  for (let i: number = 0; i < s.length; i++) {
-    if (hashset.has(s.charAt(i))) {
-      hashset.delete(s.charAt(i));
-    } else {
-      hashset.add(s.charAt(i));
+  let count = 0;
+  // loop through the 128 Ascii characters from 0 - 127
+  for (let i: number = 0; i < 128; i++) {
+    let characterFreq = 0;
+    for (let j: number = 0; j < s.length; j++) {
+      if (s.charCodeAt(j) === i) {
+        characterFreq++;
+      }
     }
+    count += characterFreq % 2;
   }
-
-  console.log(hashset);
-
-  return hashset.size >= 1;
+  return count <= 1;
 }
 
-const s = 'code';
+const s = 'carerac';
 
 console.log(canPermutePalindrome(s));
