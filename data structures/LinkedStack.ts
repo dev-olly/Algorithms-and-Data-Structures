9eccfba2eb4112bab203f53d7dd3652de9b69ad7
Olly <developer.olly@gmail.com>
2022-02-06 16:10:34 +0100

    ds: implemented stack

--- data structures/LinkedStack.ts
+++ data structures/LinkedStack.ts
@@ -1,30 +1,36 @@
 import LinkedList from './LinkedList';
 
 class LinkedStack<T> {
-  list: LinkedList<T> = null;
+  #list: LinkedList<T> = null;
 
   constructor() {
-    this.list = new LinkedList();
+    this.#list = new LinkedList();
   }
 
   push(item: T): LinkedList<T> {
-    this.list.addFirst(item);
-    return this.list;
+    this.#list.addFirst(item);
+    return this.#list;
   }
 
   pop(): LinkedList<T> {
-    this.list.popFirst();
-    return this.list;
+    this.#list.popFirst();
+    return this.#list;
   }
 
   isEmpty(): boolean {
-    return this.list.isEmpty();
+    return this.#list.isEmpty();
+  }
+
+  toArray(): T[] {
+    return this.#list.toArray();
   }
 }
 
 const stack: LinkedStack<number> = new LinkedStack();
 
-console.log(stack.push(1));
+stack.push(1);
 stack.push(2);
 stack.push(3);
 stack.push(4);
+
+console.log(stack.toArray());
