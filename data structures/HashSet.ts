4601b02cb6b4bf937e02932cd8bf7f6919c22162
Olly <developer.olly@gmail.com>
2022-02-07 16:09:31 +0100

    ds: done with hashset

--- data structures/HashSet.ts
+++ data structures/HashSet.ts
@@ -1,1 +1,32 @@
-class HashSet {}
+class HashSet {
+  set: number[] = [];
+  constructor() {}
+
+  add(key: number): void {
+    if (!this.contains(key)) {
+      this.set.push(key);
+    }
+  }
+
+  remove(key: number): void {
+    if (this.contains(key)) {
+      const index = this.set.indexOf(key);
+      this.set.splice(index, 1);
+    }
+  }
+
+  contains(key: number): boolean {
+    const index = this.set.indexOf(key);
+    return index >= 0;
+  }
+}
+
+const list = new HashSet();
+
+list.add(1);
+list.add(2);
+list.add(3);
+
+list.remove(2);
+
+console.log(list.contains(1));
