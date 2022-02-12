803c3bbe053f18c05ad0513611305cfcfc041785
Olly <developer.olly@gmail.com>
2022-02-12 22:53:10 +0100

    hr: updated separate numbers

--- hackerrank/separateNumbers.ts
+++ hackerrank/separateNumbers.ts
@@ -1,7 +1,7 @@
 function separateNumbers(s: string): void {
   // Write your code here
 
-  for (let i: number = 0; i <= Math.floor(s.length / 2); i++) {
+  for (let i: number = 0; i < Math.floor(s.length / 2); i++) {
     let sub = BigInt(s.slice(0, i + 1));
     const first = sub.toString();
     let newStr = `${sub}`;
@@ -11,6 +11,8 @@ function separateNumbers(s: string): void {
       newStr += sub;
     }
     if (newStr === s) {
+      console.log('YES', first);
+
       return;
     }
   }
@@ -19,5 +21,5 @@ function separateNumbers(s: string): void {
   return;
 }
 
-let s: string = '90071992547409929007199254740993';
+let s: string = '13';
 separateNumbers(s);
