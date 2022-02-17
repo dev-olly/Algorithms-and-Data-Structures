094bf3022b91ff30fa86ad4ffc6e7df7a8b5fc7a
Olly <developer.olly@gmail.com>
2022-02-17 22:06:25 +0100

    ds: init queue

--- data structures/CircularQueue.ts
+++ data structures/CircularQueue.ts
@@ -1,64 +1,38 @@
 class MyCircularQueue {
   list: number[];
-  head: number = 0;
-  tail: number = 0;
+  head: number = -1;
+  tail: number = -1;
   k: number;
   constructor(k: number) {
     this.list = new Array(k).fill(null);
     this.k = k;
   }
 
-  enQueue(value: number): boolean {
-    if (this.tail === this.head && this.list[this.head] !== null) {
-      return false;
-    }
-    this.list[this.tail] = value;
-    this.tail = (this.tail + 1) % this.k;
-    return true;
-  }
+  enQueue(value: number): boolean {}
 
-  deQueue(): boolean {
-    if (this.list[this.head] === null) {
-      return false;
-    }
-    this.list[this.head] = null;
-    this.head = (this.head + 1) % this.k;
-    return true;
-  }
+  deQueue(): boolean {}
 
-  Front(): number {
-    if (this.list[this.head] === null) return -1;
-    return this.list[this.head];
-  }
+  Front(): number {}
 
-  Rear(): number {
-    if (this.list[this.tail] === null) return -1;
-    return this.list[this.tail];
-  }
+  Rear(): number {}
 
-  isEmpty(): boolean {
-    return (
-      this.list[this.tail] == this.list[this.head] &&
-      this.list[this.tail] === null
-    );
-  }
+  isEmpty(): boolean {}
 
-  isFull(): boolean {
-    return (
-      this.list[this.tail] == this.list[this.head] &&
-      this.list[this.tail] !== null
-    );
-  }
+  isFull(): boolean {}
 }
 
 const queue = new MyCircularQueue(3);
-console.log(queue.isEmpty());
+// console.log(queue.isEmpty());
 
 queue.enQueue(1);
 queue.enQueue(2);
 queue.enQueue(3);
+queue.enQueue(4);
 
-console.log(queue.isFull());
+console.log('rear is', queue.Rear());
+console.log(queue.list);
+
+// console.log(queue.isFull());
 
 // console.log(queue.enQueue(4));
 
