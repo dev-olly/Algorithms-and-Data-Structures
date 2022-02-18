1860ef2bc0098e4f8784aad9db2896816cea5fdf
Olly <developer.olly@gmail.com>
2022-02-18 21:05:50 +0100

    ds: perfected the queue

--- data structures/CircularQueue.ts
+++ data structures/CircularQueue.ts
@@ -8,35 +8,80 @@ class MyCircularQueue {
     this.k = k;
   }
 
-  enQueue(value: number): boolean {}
+  enQueue(value: number): boolean {
+    if (this.tail < 0 && this.head < 0) {
+      this.tail++;
+      this.head++;
+      this.list[this.tail] = value;
+      return true;
+    }
 
-  deQueue(): boolean {}
+    const tail = (this.tail + 1) % this.k;
+    if (tail === this.head && this.list[this.head] !== null) {
+      return false;
+    }
+    this.list[tail] = value;
+    this.tail = tail;
+    return true;
+  }
 
-  Front(): number {}
+  deQueue(): boolean {
+    if (this.head < 0) return false;
+    if (this.list[this.head] === null) {
+      return false;
+    }
+    this.list[this.head] = null;
+    this.head = (this.head + 1) % this.k;
+    return true;
+  }
 
-  Rear(): number {}
+  Front(): number {
+    if (this.head < 0) return -1;
+    if (this.list[this.head] === null) return -1;
+    return this.list[this.head];
+  }
 
-  isEmpty(): boolean {}
+  Rear(): number {
+    if (this.head < 0) return -1;
+    if (this.list[this.tail] === null) return -1;
+    return this.list[this.tail];
+  }
 
-  isFull(): boolean {}
+  isEmpty(): boolean {
+    if (this.head < 0) return true;
+    for (let i: number = 0; i < this.list.length; i++) {
+      if (this.list[i] !== null) return false;
+    }
+
+    return true;
+  }
+
+  isFull(): boolean {
+    for (let i: number = 0; i < this.list.length; i++) {
+      if (this.list[i] === null) return false;
+    }
+
+    return true;
+  }
 }
 
 const queue = new MyCircularQueue(3);
-// console.log(queue.isEmpty());
+console.log(queue.isEmpty());
 
 queue.enQueue(1);
 queue.enQueue(2);
 queue.enQueue(3);
 queue.enQueue(4);
+console.log(queue.deQueue());
 
 console.log('rear is', queue.Rear());
+console.log(queue.Front());
+
 console.log(queue.list);
 
 // console.log(queue.isFull());
 
 // console.log(queue.enQueue(4));
-
-// console.log(queue.deQueue());
 
 // console.log(queue.Front());
 
