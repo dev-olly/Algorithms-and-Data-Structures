375ab13ee5a31a760b7ae0e0b9ebf478ef90129e
Olly <developer.olly@gmail.com>
2022-02-20 23:24:30 +0100

    lc: reformat date

---
+++ leetcode/reformatDate.ts
@@ -0,0 +1,30 @@
+function reformatDate(date: string): string {
+  const month = new Map<string, string>();
+  month.set('Jan', '01');
+  month.set('Feb', '02');
+  month.set('Mar', '03');
+  month.set('Apr', '04');
+  month.set('May', '05');
+  month.set('Jun', '06');
+  month.set('Jul', '07');
+  month.set('Aug', '08');
+  month.set('Sep', '09');
+  month.set('Oct', '10');
+  month.set('Nov', '11');
+  month.set('Dec', '12');
+
+  const arr: string[] = date.split(' ');
+  let day = arr[0];
+  if (day.length === 3) {
+    day = `0${day.charAt(0)}`;
+  } else {
+    day = day.substring(0, 2);
+  }
+  let str = `${arr[2]}-${month.get(arr[1])}-${day}`;
+
+  return str;
+}
+
+const date = '20th Oct 2052';
+
+console.log(reformatDate(date));
