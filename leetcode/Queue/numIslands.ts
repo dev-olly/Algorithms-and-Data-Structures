731c32504254b41b87983491e3fe17953510be4c
Olly <developer.olly@gmail.com>
2022-02-24 22:28:22 +0100

    lc: numsIsland using array as queue

--- leetcode/Queue/numIslands.ts
+++ leetcode/Queue/numIslands.ts
@@ -1,44 +1,85 @@
-import LinkedList from '../../data structures/LinkedList';
+// import LinkedList from '../../data structures/LinkedList';
+
+// function numIslands(grid: string[][]): number {
+//   const land = '1';
+//   const water = '0';
+//   const rows = grid.length;
+//   const cols = grid[0].length;
+//   let islands = 0;
+//   for (let i: number = 0; i < rows; i++) {
+//     for (let j: number = 0; j < cols; j++) {
+//       if (grid[i][j] === land) {
+//         islands++;
+
+//         const queue: LinkedList<number[]> = new LinkedList();
+//         queue.addLast([i, j]);
+//         grid[i][j] = water;
+//         while (!queue.isEmpty()) {
+//           let [row, col] = queue.popFirst();
+
+//           if (row - 1 >= 0 && grid[row - 1][col] === land) {
+//             grid[row - 1][col] = water;
+//             queue.addLast([row - 1, col]);
+//           }
+//           if (row + 1 < rows && grid[row + 1][col] === land) {
+//             grid[row + 1][col] = water;
+//             queue.addLast([row + 1, col]);
+//           }
+//           if (col + 1 < cols && grid[row][col + 1] === land) {
+//             grid[row][col + 1] = water;
+//             queue.addLast([row, col + 1]);
+//           }
+//           if (col - 1 >= 0 && grid[row][col - 1] === land) {
+//             grid[row][col - 1] = water;
+//             queue.addLast([row, col - 1]);
+//           }
+//         }
+//       }
+//     }
+//   }
+
+//   return islands;
+// }
 
 function numIslands(grid: string[][]): number {
   const land = '1';
   const water = '0';
   const rows = grid.length;
   const cols = grid[0].length;
-  let islands = 0;
+  let island = 0;
   for (let i: number = 0; i < rows; i++) {
     for (let j: number = 0; j < cols; j++) {
       if (grid[i][j] === land) {
-        islands++;
+        island++;
 
-        const queue: LinkedList<number[]> = new LinkedList();
-        queue.addLast([i, j]);
+        const queue: number[][] = [];
+        queue.push([i, j]);
         grid[i][j] = water;
-        while (!queue.isEmpty()) {
-          let [row, col] = queue.popFirst();
+        while (queue.length !== 0) {
+          let [row, col] = queue.pop();
 
           if (row - 1 >= 0 && grid[row - 1][col] === land) {
             grid[row - 1][col] = water;
-            queue.addLast([row - 1, col]);
+            queue.unshift([row - 1, col]);
           }
           if (row + 1 < rows && grid[row + 1][col] === land) {
             grid[row + 1][col] = water;
-            queue.addLast([row + 1, col]);
+            queue.unshift([row + 1, col]);
           }
           if (col + 1 < cols && grid[row][col + 1] === land) {
             grid[row][col + 1] = water;
-            queue.addLast([row, col + 1]);
+            queue.unshift([row, col + 1]);
           }
           if (col - 1 >= 0 && grid[row][col - 1] === land) {
             grid[row][col - 1] = water;
-            queue.addLast([row, col - 1]);
+            queue.unshift([row, col - 1]);
           }
         }
       }
     }
   }
 
-  return islands;
+  return island;
 }
 
 const grid = [
