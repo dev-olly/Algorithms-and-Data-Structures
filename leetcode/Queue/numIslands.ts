231dd08e496e46b49ca7c1395cdbfde3fdc65dda
Olly <developer.olly@gmail.com>
2022-02-24 21:32:45 +0100

    lc: num islands

--- leetcode/Queue/numIslands.ts
+++ leetcode/Queue/numIslands.ts
@@ -5,53 +5,47 @@ function numIslands(grid: string[][]): number {
   const water = '0';
   const rows = grid.length;
   const cols = grid[0].length;
-  const queue: LinkedList<number[]> = new LinkedList();
-  const set = new Set<number[]>();
   let islands = 0;
   for (let i: number = 0; i < rows; i++) {
     for (let j: number = 0; j < cols; j++) {
       if (grid[i][j] === land) {
+        islands++;
+
+        const queue: LinkedList<number[]> = new LinkedList();
         queue.addLast([i, j]);
+        grid[i][j] = water;
+        while (!queue.isEmpty()) {
+          let [row, col] = queue.popFirst();
+
+          if (row - 1 >= 0 && grid[row - 1][col] === land) {
+            grid[row - 1][col] = water;
+            queue.addLast([row - 1, col]);
+          }
+          if (row + 1 < rows && grid[row + 1][col] === land) {
+            grid[row + 1][col] = water;
+            queue.addLast([row + 1, col]);
+          }
+          if (col + 1 < cols && grid[row][col + 1] === land) {
+            grid[row][col + 1] = water;
+            queue.addLast([row, col + 1]);
+          }
+          if (col - 1 >= 0 && grid[row][col - 1] === land) {
+            grid[row][col - 1] = water;
+            queue.addLast([row, col - 1]);
+          }
+        }
       }
     }
   }
-  if (queue.isEmpty()) {
-    return islands;
-  }
 
-  console.log(queue.toArray());
-
-  while (!queue.isEmpty) {
-    const [row, col] = queue.popFirst();
-    set.add([row, col]);
-
-    // get all directions
-    const directions = [];
-    if (row > 0) {
-      directions.push([row - 1, col]);
-    }
-    if (row < rows - 1) {
-      directions.push([row + 1, col]);
-    }
-    if (col > 0) {
-      directions.push([row, col - 1]);
-    }
-    if (col < cols - 1) {
-      directions.push([row, col + 1]);
-    }
-
-    for(let i: number = 0; i < directions.length; i++) {
-     
-    }
-  }
-  return 1;
+  return islands;
 }
 
 const grid = [
-  ['1', '1', '1', '1', '0'],
-  ['1', '1', '0', '1', '0'],
   ['1', '1', '0', '0', '0'],
-  ['0', '0', '0', '0', '0'],
+  ['1', '1', '0', '0', '0'],
+  ['0', '0', '1', '0', '0'],
+  ['0', '0', '0', '1', '1'],
 ];
 
 console.log(numIslands(grid));
