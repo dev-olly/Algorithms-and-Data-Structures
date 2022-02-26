01c58e6a987cf56624ead67608b33f3ee4dec3b9
Olly <developer.olly@gmail.com>
2022-02-26 21:51:44 +0100

    lc: daily temperature

--- leetcode/dailyTemperatures.ts
+++ leetcode/dailyTemperatures.ts
@@ -1,12 +1,30 @@
+// function dailyTemperatures(temperatures: number[]): number[] {
+//   const output: number[] = new Array(temperatures.length).fill(0);
+//   for (let i: number = 0; i < temperatures.length; i++) {
+//     for (let j: number = i + 1; j < temperatures.length; j++) {
+//       if (temperatures[j] > temperatures[i]) {
+//         output[i] = j - i;
+//         break;
+//       }
+//     }
+//   }
+//   return output;
+// }
+
 function dailyTemperatures(temperatures: number[]): number[] {
   const output: number[] = new Array(temperatures.length).fill(0);
+  const stack: number[] = [];
+
   for (let i: number = 0; i < temperatures.length; i++) {
-    for (let j: number = i + 1; j < temperatures.length; j++) {
-      if (temperatures[j] > temperatures[i]) {
-        output[i] = j - i;
-        break;
-      }
+    while (
+      stack.length !== 0 &&
+      temperatures[stack[stack.length - 1]] < temperatures[i]
+    ) {
+      const previousDay = stack.pop();
+      output[previousDay] = i - previousDay;
     }
+
+    stack.push(i);
   }
   return output;
 }
