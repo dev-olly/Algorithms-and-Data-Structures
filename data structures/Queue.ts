1cad64c6e13d6d4fcba6d1ed137ec5c6e65a9dd3
Olly <developer.olly@gmail.com>
2022-03-09 23:47:58 +0100

    ds: Queue data structure

--- data structures/Queue.ts
+++ data structures/Queue.ts
@@ -1,7 +1,39 @@
-interface IQueue<T> {
-  œ;
-}
+import LinkedList from './LinkedList';
 
 class Queue<T> {
-  constructor(parameters) {}
+  #list: LinkedList<T>;
+  constructor() {
+    this.#list = new LinkedList();
+  }
+
+  top(): T {
+    return this.#list.head.value;
+  }
+
+  bottom(): T {
+    return this.#list.tail.value;
+  }
+  push(item: T): LinkedList<T> {
+    this.#list.addLast(item);
+    return this.#list;
+  }
+
+  pop(): LinkedList<T> {
+    this.#list.popFirst();
+    return this.#list;
+  }
+
+  isEmpty(): boolean {
+    return this.#list.isEmpty();
+  }
+
+  toArray(): T[] {
+    return this.#list.toArray();
+  }
+
+  size(): number {
+    return this.#list.size();
+  }
 }
+
+export default Queue;
