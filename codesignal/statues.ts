4dd4bb23dffb35fa590226e4f0bf23db1f328954
Olly <developer.olly@gmail.com>
2022-03-15 16:46:06 +0100

    cs: status linear time complexity

--- codesignal/statues.ts
+++ codesignal/statues.ts
@@ -1,17 +1,29 @@
+// function solution(statues: number[]): number {
+//   statues.sort((a, b) => a - b);
+
+//   let count = 0;
+
+//   for (let i: number = 0; i < statues.length - 1; i++) {
+//     let next = statues[i] + 1;
+//     while (next !== statues[i + 1]) {
+//       next++;
+//       count++;
+//     }
+//   }
+
+//   return count;
+// }
+
 function solution(statues: number[]): number {
-  statues.sort((a, b) => a - b);
+  let max = -1;
+  let min = Number.MAX_VALUE;
 
-  let count = 0;
-
-  for (let i: number = 0; i < statues.length - 1; i++) {
-    let next = statues[i] + 1;
-    while (next !== statues[i + 1]) {
-      next++;
-      count++;
-    }
+  for (let i: number = 0; i < statues.length; i++) {
+    max = Math.max(max, statues[i]);
+    min = Math.min(min, statues[i]);
   }
 
-  return count;
+  return max - min - statues.length + 1;
 }
 
 const statues = [6, 2, 3, 8];
