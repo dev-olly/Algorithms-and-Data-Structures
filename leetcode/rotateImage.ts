bfce7292f3f9deee0204665553e49b0f953a6d9f
Olly <developer.olly@gmail.com>
2022-03-17 23:06:13 +0100

    lc: modified rotate image to inplace

--- leetcode/rotateImage.ts
+++ leetcode/rotateImage.ts
@@ -1,28 +1,47 @@
+// function rotate(matrix: number[][]): void {
+//   let mat = [];
+
+//   const rows = matrix.length - 1;
+//   const cols = matrix[0].length - 1;
+
+//   for (let i: number = 0; i <= rows; i++) {
+//     mat.push(new Array(cols + 1).fill(0));
+//   }
+
+//   for (let i: number = 0; i <= rows; i++) {
+//     for (let j: number = 0; j <= cols; j++) {
+//       mat[j][rows - i] = matrix[i][j];
+//     }
+//   }
+
+//   for (let i: number = 0; i <= rows; i++) {
+//     for (let j: number = 0; j <= cols; j++) {
+//       matrix[i][j] = mat[i][j];
+//     }
+//   }
+
+//   //   matrix = [...mat];
+
+//   console.log(matrix);
+// }
+
 function rotate(matrix: number[][]): void {
-  let mat = [];
+  const n = matrix.length;
 
-  const rows = matrix.length - 1;
-  const cols = matrix[0].length - 1;
+  for (let i: number = 0; i < Math.floor(n / 2) + (n % 2); i++) {
+    for (let j: number = 0; j < Math.floor(n / 2); j++) {
+      console.log(i, j);
 
-  for (let i: number = 0; i <= rows; i++) {
-    mat.push(new Array(cols + 1).fill(0));
-  }
+      const x = n - i - 1;
+      const y = n - j - 1;
 
-  for (let i: number = 0; i <= rows; i++) {
-    for (let j: number = 0; j <= cols; j++) {
-      mat[j][rows - i] = matrix[i][j];
+      const temp = matrix[y][i];
+      matrix[y][i] = matrix[x][y];
+      matrix[x][y] = matrix[j][x];
+      matrix[j][x] = matrix[i][j];
+      matrix[i][j] = temp;
     }
   }
-
-  for (let i: number = 0; i <= rows; i++) {
-    for (let j: number = 0; j <= cols; j++) {
-      matrix[i][j] = mat[i][j];
-    }
-  }
-
-  //   matrix = [...mat];
-
-  console.log(matrix);
 }
 
 const matrix = [
@@ -31,4 +50,4 @@ const matrix = [
   [7, 8, 9],
 ];
 
-console.log(rotate(matrix));
+rotate(matrix);
