bec57f8f1e528996f1809b7e821475af6458226c
Olly <developer.olly@gmail.com>
2022-03-18 22:49:57 +0100

    lc: rotate image using transpose and rotate

--- leetcode/rotateImage.ts
+++ leetcode/rotateImage.ts
@@ -25,23 +25,51 @@
 //   console.log(matrix);
 // }
 
+// function rotate(matrix: number[][]): void {
+//   const n = matrix.length;
+
+//   for (let i: number = 0; i < Math.floor(n / 2) + (n % 2); i++) {
+//     for (let j: number = 0; j < Math.floor(n / 2); j++) {
+//       const x = n - i - 1;
+//       const y = n - j - 1;
+
+//       const temp = matrix[y][i];
+//       console.log(temp);
+
+//       matrix[y][i] = matrix[x][y];
+//       matrix[x][y] = matrix[j][x];
+//       matrix[j][x] = matrix[i][j];
+//       matrix[i][j] = temp;
+//     }
+//   }
+
+//   console.log(matrix);
+// }
+
 function rotate(matrix: number[][]): void {
   const n = matrix.length;
 
-  for (let i: number = 0; i < Math.floor(n / 2) + (n % 2); i++) {
-    for (let j: number = 0; j < Math.floor(n / 2); j++) {
-      console.log(i, j);
-
-      const x = n - i - 1;
-      const y = n - j - 1;
-
-      const temp = matrix[y][i];
-      matrix[y][i] = matrix[x][y];
-      matrix[x][y] = matrix[j][x];
-      matrix[j][x] = matrix[i][j];
-      matrix[i][j] = temp;
+  function transpose() {
+    for (let i: number = 0; i < n; i++) {
+      for (let j: number = i + 1; j < n; j++) {
+        [matrix[i][j], matrix[j][i]] = [matrix[j][i], matrix[i][j]];
+      }
     }
   }
+
+  function rotate() {
+    for (let i: number = 0; i < n; i++) {
+      for (let j: number = 0; j < Math.floor(n / 2); j++) {
+        const y = n - j - 1;
+        [matrix[i][j], matrix[i][y]] = [matrix[i][y], matrix[i][j]];
+      }
+    }
+  }
+
+  transpose();
+  rotate();
+
+  console.log(matrix);
 }
 
 const matrix = [
@@ -49,5 +77,12 @@ const matrix = [
   [4, 5, 6],
   [7, 8, 9],
 ];
+
+// const matrix = [
+//   [5, 1, 9, 11],
+//   [2, 4, 8, 10],
+//   [13, 3, 6, 7],
+//   [15, 14, 12, 16],
+// ];
 
 rotate(matrix);
