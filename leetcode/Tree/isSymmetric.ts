6df45b031ba1b37554a1bd1e7ed5d059eab37f2b
Olly <developer.olly@gmail.com>
2022-03-31 23:29:55 +0100

    lc: is symmetric iterative solution

--- leetcode/Tree/isSymmetric.ts
+++ leetcode/Tree/isSymmetric.ts
@@ -1,3 +1,5 @@
+import LinkedList from '../../data structures/LinkedList';
+
 class TreeNode {
   val: number;
   left: TreeNode | null;
@@ -11,22 +13,36 @@ class TreeNode {
 
 function isSymmetric(root: TreeNode | null): boolean {
   if (root === null) return true;
-  return isMirror(root.left, root.right);
-}
+  const stack: TreeNode[][] = [[root.left, root.right]];
 
-function isMirror(left: TreeNode, right: TreeNode) {
-  if (!left && !right) return true;
-  if (!left || !right) return false;
-
-  if (left.val === right.val) {
-    const outpair = isMirror(left.left, right.right);
-    const inpair = isMirror(left.right, right.left);
-
-    return outpair && inpair;
+  while (stack.length !== 0) {
+    const [left, right] = stack.pop();
+    if (!left && !right) continue;
+    if (!left || !right) return false;
+    if (left.val === right.val) {
+      stack.push([left.left, right.right]);
+      stack.push([left.right, right.left]);
+    } else {
+      return false;
+    }
   }
 
-  return false;
+  return true;
 }
+
+// function isMirror(left: TreeNode, right: TreeNode) {
+//   if (!left && !right) return true;
+//   if (!left || !right) return false;
+
+//   if (left.val === right.val) {
+//     const outpair = isMirror(left.left, right.right);
+//     const inpair = isMirror(left.right, right.left);
+
+//     return outpair && inpair;
+//   }
+
+//   return false;
+// }
 
 const root = new TreeNode(
   1,
